2cb8b0163f951f92038a6c58dc3d35fb27e0ec5d
vitrigovm <vitrigovm@gmail.com>
2024-05-21 23:38:42 +0800

    добавить канвас и реализовать заполнение случайным цветом по клику по нему

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
+
+const getRandomNumber = (from: number, to: number) => {
+    return Math.floor(Math.random() * to) + from;
+};
+
+const getRandomColor = () => {
+    const red = getRandomNumber(0, 256);
+    const green = getRandomNumber(0, 256);
+    const blue = getRandomNumber(0, 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+};
+
+function Main() {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [context, setContext] = useState<CanvasRenderingContext2D | null | undefined>(null);
+
+    useEffect(() => {
+        setContext(canvasRef.current?.getContext('2d'));
+    }, []);
+
+    const handleFillGreen = () => {
+        if (context) {
+            console.log(getRandomColor());
+            context.fillStyle = getRandomColor();
+            context.fillRect(0, 0, 500, 500);
+        }
+    };
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <button onClick={handleFillGreen}>fill</button>
+
+            <canvas
+                ref={canvasRef}
+                style={{ background: '#fff' }}
+                id="myCanvas"
+                width="500"
+                height="500"
+            />
+        </div>
+    );
+}
+
+export default Main;
